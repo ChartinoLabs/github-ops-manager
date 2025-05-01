9a0b4d5d3ab79d9d1d0ed1409d194f1d1b28aa75
Christopher Hart <chart2@cisco.com>
2025-04-30 21:01:43 -0400

    chore: remove obsolete unit test file for configuration.reconcile module

--- tests/unit/test_configuration_reconcile.py
+++
@@ -1,8 +0,0 @@
-"""Unit tests for the configuration.reconcile module."""
-
-import pytest
-
-from github_ops_manager.configuration.exceptions import (
-    GitHubAuthenticationConfigurationUndefinedError,
-)
-from github_ops_manager.configuration.reconcile import validate_github_authentication_configuration
