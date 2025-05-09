44dc2437fa0f4e29e8f0e1bd6196f97bf5902076
Christopher Hart <christopherjhart95@gmail.com>
2025-05-08 21:57:31 -0400

    chore: remove empty  files

--- github_ops_manager/synchronize/workflow_runner.py
+++
@@ -1,8 +0,0 @@
-# This file is intended to orchestrate the main actions (e.g., sync-to-github, export-issues).
-
-"""Orchestrates the main workflows (e.g., sync-to-github, export-issues)."""
-
-import structlog
-from structlog.stdlib import BoundLogger
-
-logger: BoundLogger = structlog.get_logger(__name__)  # type: ignore
