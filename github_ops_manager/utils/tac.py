deff3b856bf965f2c0dc59743e40fe704300ff70
Christopher Hart <chart2@cisco.com>
2025-06-25 17:43:17 -0400

    chore: add utility functions for test case definition searching

---
+++ github_ops_manager/utils/tac.py
@@ -0,0 +1,26 @@
+"""Contains utility functions for interacting with Testing as Code constructs."""
+
+from github_ops_manager.schemas.tac import TestingAsCodeTestCaseDefinition, TestingAsCodeTestCaseDefinitions
+
+
+def find_test_case_definition_with_file(test_case_definitions: TestingAsCodeTestCaseDefinitions, file: str) -> TestingAsCodeTestCaseDefinition | None:
+    """Finds a test case definition that resulted in a specific created file."""
+    for test_case_definition in test_case_definitions.test_cases:
+        if test_case_definition.resulting_filename == file:
+            return test_case_definition
+    return None
+
+
+def find_test_case_definition_with_files(
+    test_case_definitions: TestingAsCodeTestCaseDefinitions, files: list[str]
+) -> TestingAsCodeTestCaseDefinition | None:
+    """Finds a test case definition that resulted in a specific created file.
+
+    This takes in a list of files; the first test case definition that contains
+    one of any of the files is returned.
+    """
+    for file in files:
+        test_case_definition = find_test_case_definition_with_file(test_case_definitions, file)
+        if test_case_definition is not None:
+            return test_case_definition
+    return None
